0b071682f9b480808ef415111d8501eee1047c53
b45i <amalshajan2011@gmail.com>
2020-10-02 08:03:02 +0530

    ag-grid setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ag-grid-test';
+  columnDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ];
 }
